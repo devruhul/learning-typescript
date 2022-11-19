c38aa94bfb21b74fa488a8404f67e72059cf7cab
devruhul <ruhulamin262001@gmail.com>
2022-11-19 07:51:05 +0600

    learn anytype

---
+++ user_defined_type/any_type.js
@@ -0,0 +1,7 @@
+// any type
+var anyType;
+anyType = "any type";
+anyType = false;
+console.log(anyType);
+anyType = { name: "any type" };
+anyType = [1, 2, 3];
---
+++ user_defined_type/any_type.ts
@@ -0,0 +1,7 @@
+// any type
+let anyType: any;
+anyType = "any type";
+anyType = false;
+console.log(anyType);
+anyType = { name: "any type" };
+anyType = [1, 2, 3];
