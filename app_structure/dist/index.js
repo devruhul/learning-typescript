0ecb7cd0a8659860ab3da9b27254298b2c65696e
devruhul <ruhulamin262001@gmail.com>
2022-11-25 22:02:14 +0600

    learned dom manipulation

--- app_structure/dist/index.js
+++ app_structure/dist/index.js
@@ -1,5 +1,16 @@
 "use strict";
-function showUserName() {
-    console.log("userName ruhul amin");
-}
-showUserName();
+// function showUserName() {
+//   console.log("userName ruhul amin");
+// }
+// showUserName();
+var userForm = document.querySelector("form");
+var userName = document.querySelector("#user-name");
+var userEmail = document.querySelector("#user-email");
+userForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var data = {
+        name: userName.value,
+        email: userEmail.value,
+    };
+    console.log(data);
+});
--- app_structure/src/index.ts
+++ app_structure/src/index.ts
@@ -1,5 +1,21 @@
-function showUserName() {
-  console.log("userName ruhul amin");
-}
+// function showUserName() {
+//   console.log("userName ruhul amin");
+// }
 
-showUserName();
+// showUserName();
+
+const userForm = document.querySelector("form") as HTMLFormElement;
+
+const userName = document.querySelector("#user-name") as HTMLInputElement;
+
+const userEmail = document.querySelector("#user-email") as HTMLInputElement;
+
+userForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const data = {
+    name: userName.value,
+    email: userEmail.value,
+  };
+
+  console.log(data);
+});
